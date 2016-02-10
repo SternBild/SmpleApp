0be35a7dd33b62eda5bc97fd25150fae0b65e4d3
SternBild <lunatickcell@gmail.com>
2016-02-10 23:46:23 +0900

    てーぶるそーとwithめたでーた

---
+++ tablesort/tablesort/www/scripts/index.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    // 1番目のth要素: 1列目をソート
+    $('#sampleTable thead th:eq(0)').addClass('{ sortIndex: 0 }');
+
+    // 2番目のth要素: 4列目をソート
+    // 使わないので余ったインデックスを割り振る
+    $('#sampleTable thead th:eq(1)').addClass('{ sortIndex: 0 }');
+
+    // 3番目のth要素: 2列目をソート
+    $('#sampleTable thead th:eq(2)').addClass('{ sortIndex: 0 }');
+
+    // 4番目のth要素: 3列目をソート
+    $('#sampleTable thead thead th:eq(3)').addClass('{ sortIndex: 0 }');
+    $("#sampleTable").tablesorter({
+        cssAsc: 'headerSortUp',
+        cssDesc: 'headerSortDown',
+        headers: {
+            0: { sorter: false }
+        }
+    });
+});
